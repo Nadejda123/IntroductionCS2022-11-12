99f93d167d1d0e93afd2ebfb26f0cd1305c722b1
«Nadejda <Nadejdaburca2@gmail.com>
2022-11-18 01:27:50 +0200

    leason 1

---
+++ 012/Program.cs
@@ -0,0 +1,7 @@
+﻿//12. С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+
+System.Console.WriteLine("Введите значение a: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int MaxNumber = a%10;
+if (a/10>MaxNumber) MaxNumber = a/10;
+System.Console.WriteLine(MaxNumber);
