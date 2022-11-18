94969906e58e1e82cb2fd4f8f7ded7a347862d7f
«Nadejda <Nadejdaburca2@gmail.com>
2022-11-18 08:49:42 +0200

    leason 1

---
+++ 014/Program.cs
@@ -0,0 +1,15 @@
+﻿// 14. С клавиатуры вводятся два числа a и b. Выяснить, кратно ли число a числу b, если нет, вывести остаток от деления a на b.
+
+System.Console.WriteLine("Веедите значение а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ведите зачение b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a%b==0)
+{
+    System.Console.WriteLine($"{a} Кратно {b}");
+}
+else 
+{
+    System.Console.WriteLine($"{a} Некратно {b}"+ $" Остаток от деления {a%b}");
+}
---
+++ 015/Program.cs
@@ -0,0 +1,12 @@
+﻿// 15. С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO).
+System.Console.WriteLine("Введите целое число");
+int a =Convert.ToInt32(Console.ReadLine());
+//523=3
+if (a<100)
+{
+    System.Console.WriteLine("NO");
+}
+else
+{
+    System.Console.WriteLine(a/100%10);
+}
