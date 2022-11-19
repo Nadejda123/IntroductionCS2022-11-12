914de06f9fecde884c5956d1024632b4e88a2e69
«Nadejda <Nadejdaburca2@gmail.com>
2022-11-19 03:30:33 +0200

    Leasson 2

--- 022/Program.cs
+++ 022/Program.cs
@@ -2,9 +2,9 @@
 
 System.Console.WriteLine("Ведите пятизначное число: ");
 int a =Convert.ToInt32(Console.ReadLine());
-if (a>100000||a<10000) System.Console.WriteLine("Недопустимое значение");
+if (a>99999||a<10000) System.Console.WriteLine("Недопустимое значение");
 else{
-    if (a/10000==a%10) System.Console.WriteLine($"Число {a} палиндромом ");
+    if (a/10000==a%10) System.Console.WriteLine($"Число {a} палиндром ");
         else {System.Console.WriteLine($"Число {a} не является палиндромом ");
         }
     }
