1b7db577e3c64d5dca989644aeea96f136e7f262
«Nadejda <Nadejdaburca2@gmail.com>
2022-11-20 14:57:45 +0200

    Leason 3

--- 008/Program.cs
+++ 008/Program.cs
@@ -1,4 +1,5 @@
 ﻿// 08. Вывести на экран числа от -N до N
+/*
 System.Console.WriteLine("Введите значение N: ");
 int N=Convert.ToInt32(Console.ReadLine());
 int i=-N;
@@ -7,4 +8,13 @@ while (i<=N)
 {
     System.Console.Write($"{i} ");
     i=i+1;
-}
+} 
+*/
+
+// Второй Вариант
+System.Console.WriteLine("Введите значение N: ");
+int N=Convert.ToInt32(Console.ReadLine());
+
+for ( int i=-N;i>=N;i++)
+    System.Console.Write($"{i} ");
+
--- 020/Program.cs
+++ 020/Program.cs
@@ -1,5 +1,6 @@
 ﻿// 20. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
-
+// Домашняя работа 
+/*
 System.Console.WriteLine("Введите координаты точки X: ");
 double x = Convert.ToDouble(Console.ReadLine());
 System.Console.WriteLine("Введите координаты точки Y: ");
@@ -19,3 +20,37 @@ if ((x>0) && (y>0)) System.Console.WriteLine("Первая четверть");
             }
 
         }
+*/
+//Вариант решения задачи с добавлением 3 переменной 
+/*        
+System.Console.WriteLine("Введите координаты точки X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите координаты точки Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+int part =0;
+if(x>0&& y>0) part=1;
+if(x<0 && y>0) part =2;
+if (x<0 && y<0) part =3;
+if (x>0 && y<0) part =4;
+System.Console.WriteLine("Part:"+part);
+*/
+// Вариант решения с под пограмами 
+// тип имя ( аргументы)
+
+int Part(double x, double y)
+{
+    //Тело подпрограмы;
+
+if(x>0&& y>0) return 1;
+if(x<0 && y>0) return 2;
+if(x<0 && y<0) return 3;
+if(x>0 && y<0) return 4;
+return 0;
+}
+
+System.Console.WriteLine("Введите координаты точки X: ");
+double x = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine("Введите координаты точки Y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Part (x,y);
+System.Console.WriteLine(Part(x,y));
